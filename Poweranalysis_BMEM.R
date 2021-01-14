1f1bbadce2e8890253cd93aa620c3913f102ce0c
Marleen Thomas <thomas_marleen@web.de>
2021-01-14 13:14:52 +0100

    three mediators and 65 nobs

--- Poweranalysis_BMEM.R
+++ Poweranalysis_BMEM.R
@@ -21,21 +21,24 @@ library(bmem)
 
 #Mediation Model and mediation effect specification for bmem
 demo <- "
-      y ~ c*x + start(.55)*x + b1*m1 + start(.55)*m1 + b2*m2 + start(.51)*m2
+      y ~ c*x + start(.55)*x + b1*m1 + start(.55)*m1 + b2*m2 + start(.51)*m2 + b3*m3 + start(.30)*m3
       m1 ~ a1*x+start(.56)*x
       m2 ~ a2*x+start(.43)*x
+      m3 ~ a3*x+start(.30)*x
       x ~~ start(1)*x
       m1 ~~ start(1)*m1
       m2 ~~ start(1)*m2
+      m3 ~~ start(1)*m3
       y ~~ start(1)*y
       "
 #Mediation effect to conduct power analyses on
 mediation <- "
           ind1 := a1*b1
           ind2 := a2*b2
-          total := ind1 + ind2
+          ind3 := a3*b3
+          total := ind1 + ind2 +ind3
          "
 #Power Analyse via bootstrap
-power.result <- power.boot(model = demo, indirect = mediation, nobs = 50, nrep = 10, nboot = 10, alpha = 0.95, parallel = "multicore") #nrep?
+power.result <- power.boot(model = demo, indirect = mediation, nobs =65, nrep = 10, nboot = 10, alpha = 0.95, parallel = "multicore") #nrep?
 
 summary(power.result)
