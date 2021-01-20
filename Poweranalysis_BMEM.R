bae1c47bf63f938a2737bcdcfce063c35192767d
Marleen Thomas <thomas_marleen@web.de>
2021-01-20 14:12:06 +0100

    Changend it from 3 to 4 mediators; Differnce in nobs for 3 or 4 mediators is as we expected around nobs=10

--- Poweranalysis_BMEM.R
+++ Poweranalysis_BMEM.R
@@ -19,16 +19,19 @@ library(Amelia)
 library(lavaan)
 library(bmem)
 
+
 #Mediation Model and mediation effect specification for bmem
 demo <- "
-      y ~ c*x + start(.55)*x + b1*m1 + start(.55)*m1 + b2*m2 + start(.51)*m2 + b3*m3 + start(.30)*m3
+      y ~ c*x + start(.55)*x + b1*m1 + start(.55)*m1 + b2*m2 + start(.51)*m2 + b3*m3 + start(.30)*m3 + b4*m4 + start(.30)*m4
       m1 ~ a1*x+start(.56)*x
       m2 ~ a2*x+start(.43)*x
       m3 ~ a3*x+start(.30)*x
+      m4 ~ a4*x+start(.30)*x
       x ~~ start(1)*x
       m1 ~~ start(1)*m1
       m2 ~~ start(1)*m2
       m3 ~~ start(1)*m3
+      m4 ~~ start(1)*m4
       y ~~ start(1)*y
       "
 #Mediation effect to conduct power analyses on
@@ -36,13 +39,15 @@ mediation <- "
           ind1 := a1*b1
           ind2 := a2*b2
           ind3 := a3*b3
-          total := ind1 + ind2 +ind3
+          ind4 := a4*b4
+          total := ind1 +ind2 +ind3 +ind4
          "
+
 #Power Analyse via bootstrap
-power.result <- power.boot(model = demo, indirect = mediation, nobs =65, nrep = 10, nboot = 10, alpha = 0.95, parallel = "multicore") #nrep?
+power.result <- power.boot(model = demo, indirect = mediation, nobs = 95, nrep = 10, nboot = 10, alpha = 0.95, parallel = "multicore") #nrep?
 
 summary(power.result)
 
 #Biased-corrected confidence intervals
-bmem.ci.bc()
+bmem.ci.bc(par.boot = boot.object, par0 = 43, cl = 0.95)
 
