2f4e8c55658e6db5f7dabf5aa1f4db5ddd2e1b5c
Marleen Thomas <thomas_marleen@web.de>
2021-02-06 11:27:32 +0100

    Added a code to predict system time - for N= 90 and nrep = 100, nboot=1000 = 17,22 hours! on my system

--- Poweranalyse.R
+++ Poweranalyse.R
@@ -18,6 +18,7 @@ library(lavaan)
 library(bmem)
 
 
+
 # Mediation Model and mediation effect specification for bmem
 
 demo <- "
@@ -54,6 +55,7 @@ power.result <- bmem::power.boot(model = demo,
 
 summary(power.result)
 
+
 #Power curve
 nobs <- seq(10, 90, by=5)
 
@@ -73,4 +75,42 @@ bmem <- bmem.ci.bc(par.boot = result.powercurve,
                  par0 = 90, 
                  cl = 0.95)
 
-summary(bmem)
+#######################
+#Predict system times #
+#######################
+
+N <- 90 
+
+system.time(bootstrap<-bmem::power.boot(demo, indirect = mediation, N, nrep = 10, nboot = 10, parallel = 'multicore'))
+system.time(bootstrap<-bmem::power.boot(demo, indirect = mediation, N, nrep = 30, nboot = 30, parallel = 'multicore'))
+system.time(bootstrap<-bmem::power.boot(demo, indirect = mediation, N, nrep = 60, nboot = 60, parallel = 'multicore'))
+system.time(bootstrap<-bmem::power.boot(demo, indirect = mediation, N, nrep = 100, nboot = 100, parallel = 'multicore'))
+
+library(tidyverse)
+install.packages("tibble")
+library(tibble)
+library(ggplot2)
+
+# Load the times from above into a dataframe
+benchmark <- tibble(bootstraps = c(10, 30, 60, 100),
+                    times = c(8.133, 43.086, 178.412, 544.586)) 
+
+# Plot the points and fit a curve
+ggplot(benchmark, aes(x = bootstraps, y = times)) +
+   geom_point() +
+   geom_smooth(se = FALSE, span = 5)
+
+# Fit a model
+fit <- lm(data = benchmark, times~poly(bootstraps,
+                                       2, raw=TRUE))
+newtimes <- data.frame(bootstraps = seq(100, 1000, length = 4))
+
+# Predict the time it will take for larger bootstrap/rep values
+predict(fit, newdata = newtimes)
+>        1          2          3          4 
+>  544.0899  9697.4746 30191.6173 62026.5178 
+
+# Convert from seconds to hours
+print(6290525.94 /60/60)
+>[1] 17.22959
+
