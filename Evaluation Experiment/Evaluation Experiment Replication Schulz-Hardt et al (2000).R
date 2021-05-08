caed329c2b09f12da918a9c4732087c89bb6ae2b
Marleen Thomas <thomas_marleen@web.de>
2021-05-08 18:51:50 +0200

    evaluation

--- Evaluation Experiment/Evaluation Experiment Replication Schulz-Hardt et al (2000).R
+++ Evaluation Experiment/Evaluation Experiment Replication Schulz-Hardt et al (2000).R
@@ -178,16 +178,13 @@ choice_gen<-ds%>%
               group_by(gen) %>%
               count(ind_ce)  
 
+choice_gen
+
 prop.table(choice_gen$n) 
   
 
 pro_gen_score<-ds%>%
                  group_by(gen) %>%
-                 get_summary_stats(ind_ce, type= "mean_sd")
-  
-                  get_summary_stats(ind_ce, type = "mean_sd")
-  
-  
                  filter(ind_ce==1) %>%
                  count()
 
@@ -251,9 +248,9 @@ ds%>%
   count(ind_ce) 
 
 
-#######################
-## Confirmation bias ##
-#######################
+##################################
+## Confirmation bias individual ##
+##################################
 
 #How many arguments pro contract extension where chosen by each participant
 ds%>%
@@ -413,11 +410,151 @@ ind_cont_ca<-ds%>%
 colSums(ind_cont_ca,na.rm=TRUE)
 colMeans(ind_cont_ca,na.rm=TRUE)
 
+
+##############################
+## Confirmation bias groups ##
+##############################
+
+#How many arguments pro contract extension where chosen by each group
+ds%>%
+  select(grp_read01, 
+         grp_read03, 
+         grp_read05, 
+         grp_read07, 
+         grp_read09, 
+         grp_read11)%>%
+  rowSums(na.rm=TRUE) -> sum_pro_ce_grp
+
+sum(sum_pro_ce_grp)
+
+#How many arguments pro contract extension where chosen by each group
+ds%>%
+  select(grp_read02, 
+         grp_read04, 
+         grp_read06, 
+         grp_read08, 
+         grp_read10, 
+         grp_read12)%>%
+  rowSums(na.rm=TRUE) -> sum_cont_ce_grp
+
+sum(sum_cont_ce_grp)
+
+#New columns with sum pro extension and sum against extension chosen by each group 
+ds_new<-cbind(ds, sum_pro_ce_grp, sum_cont_ce_grp)
+glimpse(ds_new)
+
+#########################################################
+# Confirmation bias groups pro contract extension #
+#########################################################
+
+#Number of groups who chose to extent contract and chose pro arguments 
+ds_new%>%
+  filter(grp_ce == 1)%>%
+  select(sum_pro_ce_grp)%>%
+  colSums(na.rm=TRUE) ->pro_ce_grp_pro
+
+#Number of groups who chose to extent contract and chose conflicting arguments
+ds_new%>%
+  filter(grp_ce == 1)%>%
+  select(sum_cont_ce_grp)%>%
+  colSums(na.rm=TRUE) ->pro_ce_grp_cont
+
+#Net confirmation bias for groups pro extension 
+pro_ce_grp_pro - pro_ce_grp_cont
+
+#Proportions test of article supporting and conflicting with pre-choice pro extension
+prop.test(x = pro_ce_grp_pro, 
+          n = pro_ce_grp_pro + pro_ce_grp_cont,
+          p = 0.5, 
+          correct = FALSE)
+
+
+#Bayesian proportion test
+bf_pro_ce_grp <- BayesFactor::proportionBF(y= pro_ce_grp_pro, 
+                                       N= pro_ce_grp_pro + pro_ce_grp_cont,
+                                       p=.05)
+
+#Evidence for the alternative
+bf_pro_ce_grp
+
+#Evidence for the null
+1/bf_pro_ce_grp
+
+#############################################################
+# Confirmation bias groups against contract extension #
+#############################################################
+
+#Number of people who chose not to extent contract and chose contra arguments 
+ds_new%>%
+  filter(grp_ce == 0)%>%
+  select(sum_cont_ce_grp)%>%
+  colSums(na.rm=TRUE) ->cont_ce_grp_cont
+
+#Number of participants who chose to extent contract and chose conflicting arguments
+ds_new%>%
+  filter(grp_ce == 0)%>%
+  select(sum_pro_ce_grp)%>%
+  colSums(na.rm=TRUE) ->cont_ce_grp_pro
+
+#Net confirmation bias for participants pro extension 
+cont_ce_grp_cont - cont_ce_grp_pro
+
+#Proportions test of article supporting and conflicting with pre-choice against extension
+prop.test(x = cont_ce_grp_cont, 
+          n = cont_ce_grp_cont + cont_ce_grp_pro,
+          p = 0.5, 
+          correct = FALSE)
+
+#Bayesian proportion test
+bf_cont_ce_grp <- BayesFactor::proportionBF(y= cont_ce_grp_cont, 
+                                        N= cont_ce_grp_cont + cont_ce_grp_pro,
+                                        p=.05)
+
+#Evidence for the alternative
+bf_pro_ce_grp
+
+#Evidence for the null
+1/bf_pro_ce_grp
+
+
 ##########################
 ## Statisticized groups ##
 ##########################
 
-#statisticized groups' information search was calculated by counting how many of the articles supporting and conflicting with the subsequent group decision were requested by at least one group member.
+#statisticized groups' information search was calculated by counting how many 
+# of the articles supporting and conflicting with the subsequent group decision 
+# were requested by at least one group member.
+view(ds_new)
 
+#Number supporting articles chosen by  groups 
+
+
+
+ds_new%>%
+  filter(id_grp == 1)%>%
+  select(sum_pro_ce)%>%
+  colSums(na.rm=TRUE) 
+
+x<-1
+
+for(i in 1:nrow(ds_new)/3)
+{
+  x_for<-
+    ds_new%>%
+    filter(id_grp == x)%>% 
+    select(sum_pro_ce)%>%
+    colSums(na.rm=TRUE) 
+  
+        
+print(x_for)
+}
+
+
+
+#Number conflicting articles chosen by  groups 
+ds_new%>%
+  filter(id_grp == 1)%>%
+  select(sum_cont_ce)%>%
+  colSums(na.rm=TRUE)
 
 
